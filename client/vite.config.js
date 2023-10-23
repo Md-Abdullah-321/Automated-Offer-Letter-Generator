ecf594aab1d00d2eef8b66eeb8405349fd9d4d24
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2023-10-24 00:34:41 +0600

    bug test

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://automated-offer-letter-generator.vercel.app/",
+        secure: false
+      }
+    }
+  },
   plugins: [react()],
 })
