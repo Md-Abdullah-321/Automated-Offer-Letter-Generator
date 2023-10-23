130d8ca9d8a90a42c3711752a0e83602952036d5
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2023-10-23 23:24:45 +0600

    bug fixing

--- server/index.js
+++ server/index.js
@@ -3,35 +3,29 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 
-// app.use(cors({
-//     origin: ["http://localhost:5173"],
-//     methods: ["POST", "GET", "DELETE", "PUT"],
-//     credentials: true
-// }))
-
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.use(cors({
+    origin: "http://localhost:5173", // Update this with your actual frontend origin
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true
+}));
 
 const mongoose = require("mongoose");
 const userRouter = require("./src/routes/user.route.js");
 
-
-
 app.get('/', (req, res, next) => {
   res.json("Hello World, How are you?");
-})
+});
+
 app.use(express.json());
 app.use('/api/user', userRouter);
 
 app.listen(3000, () => {
     console.log('App is running on port 3000');
     mongoose
-        .connect(process.env.DATABASE_URL)
+        .connect(process.env.DATABASE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         .then(() => console.log("Connected to MongoDB..."))
         .catch((err) => console.error("Could not connect to MongoDB...", err));
-})
+});
